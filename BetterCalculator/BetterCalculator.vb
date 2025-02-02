0e39915a5b34952d9d086ca177e26604fab7bfe1
TheGoldenPorkchop <angelnavamusic@gmail.com>
2025-02-02 13:42:50 -0700

    Fixed Verbage to "Present the user with the following prompts exactly"

--- BetterCalculator/BetterCalculator.vb
+++ BetterCalculator/BetterCalculator.vb
@@ -2,7 +2,7 @@
 'Spring 2025
 'RCET2265
 'BetterCalculator
-'
+'https://github.com/TheGoldenPorkchop/Better-Calculator/tree/main
 
 Module BetterCalculator
 
@@ -14,88 +14,48 @@ Module BetterCalculator
         Dim quitProgram As Boolean = False
         Dim userInput As String
         Do
+            Console.WriteLine($"Please enter two numbers. Enter Q at any time to quit.")
+            'First Number
             Do
-                Console.WriteLine("Enter your first number")
-                Console.WriteLine("Enter Q to Quit")
+                Console.Write("Choose a Number: ")
                 userInput = Console.ReadLine()
-                Console.WriteLine()
 
                 Try
                     firstNumber = CInt(userInput)
                     getOutOfHere = True
-                    Console.WriteLine($"First Number = {firstNumber}")
+                    Console.WriteLine($"You entered {firstNumber}")
                 Catch ex As Exception
                     If userInput = "q" Then
-                        Console.WriteLine("You have Quit the program")
+                        Console.WriteLine("Have a nice day")
+                        Console.WriteLine("Press enter to close this window")
                         getOutOfHere = True
                         quitProgram = True
                     Else
-                        Console.WriteLine($"{userInput} is not a valid number")
-                        Console.WriteLine($"Please enter a valid number")
+                        Console.WriteLine($"You entered {userInput}, please enter a whole number.")
                         getOutOfHere = False
                     End If
                 End Try
-
             Loop Until getOutOfHere = True
 
-            Console.WriteLine()
+            'Second Number
 
             If quitProgram = False Then
                 Do
-                    Console.WriteLine("Please make a selection: " & vbNewLine _
-                      & "[1. Add]" & vbNewLine _
-                      & "[2. Subtract]" & vbNewLine _
-                      & "[3. Multiply]")
-                    Console.WriteLine("Enter Q to Quit")
-                    userInputOperation = Console.ReadLine()
-                    If userInputOperation = "q" Then
-                        Console.WriteLine("You have Quit the program")
-                        getOutOfHere = True
-                        quitProgram = True
-                    Else
-                        If userInputOperation = "1" Then
-                            Console.WriteLine("Adding")
-                            userInputOperation = "+"
-                            getOutOfHere = True
-                        ElseIf userInputOperation = "2" Then
-                            Console.WriteLine("Subtracting")
-                            userInputOperation = "-"
-                            getOutOfHere = True
-                        ElseIf userInputOperation = "3" Then
-                            Console.WriteLine("Multiplying")
-                            userInputOperation = "*"
-                            getOutOfHere = True
-                        Else
-                            Console.WriteLine("Invalid Option")
-                            getOutOfHere = False
-                        End If
-                    End If
-                    Console.WriteLine()
-
-                Loop Until getOutOfHere = True
-            End If
-
-            Console.WriteLine()
-
-            If quitProgram = False Then
-                Do
-                    Console.WriteLine("Enter your second number")
-                    Console.WriteLine("Enter Q to Quit")
+                    Console.Write("Choose a Number: ")
                     userInput = Console.ReadLine()
-                    Console.WriteLine()
 
                     Try
                         secondNumber = CInt(userInput)
                         getOutOfHere = True
-                        Console.WriteLine($"Second Number = {secondNumber}")
+                        Console.WriteLine($"You entered {secondNumber}")
                     Catch ex As Exception
                         If userInput = "q" Then
-                            Console.WriteLine("You have Quit the program")
+                            Console.WriteLine("Have a nice day")
+                            Console.WriteLine("Press enter to close this window")
                             getOutOfHere = True
                             quitProgram = True
                         Else
-                            Console.WriteLine($"{userInput} is not a valid number")
-                            Console.WriteLine($"Please enter a valid number")
+                            Console.WriteLine($"You entered {userInput}, please enter a whole number.")
                             getOutOfHere = False
                         End If
                     End Try
@@ -103,27 +63,58 @@ Module BetterCalculator
                 Loop Until getOutOfHere = True
             End If
 
-            Console.WriteLine()
+            'Operation
 
             If quitProgram = False Then
-                Console.WriteLine("Your Calculation")
-                Console.WriteLine(firstNumber & userInputOperation & secondNumber & "=")
+                Do
+                    Console.WriteLine("Choose one of the following options: " & vbNewLine _
+                      & "1. Add" & vbNewLine _
+                      & "2. Subtract" & vbNewLine _
+                      & "3. Multiply" & vbNewLine _
+                      & "4. Divide")
+                    userInputOperation = Console.ReadLine()
+                    Console.WriteLine($"You entered {userInputOperation}")
+                    If userInputOperation = "q" Then
+                        Console.WriteLine("Have a nice day")
+                        Console.WriteLine("Press enter to close this window")
+                        getOutOfHere = True
+                        quitProgram = True
+                    Else
+                        If userInputOperation = "1" Then
+                            userInputOperation = "+"
+                            getOutOfHere = True
+                        ElseIf userInputOperation = "2" Then
+                            userInputOperation = "-"
+                            getOutOfHere = True
+                        ElseIf userInputOperation = "3" Then
+                            userInputOperation = "*"
+                            getOutOfHere = True
+                        ElseIf userInputOperation = "4" Then
+                            userInputOperation = "/"
+                            getOutOfHere = True
+                        Else
+                            getOutOfHere = False
+                        End If
+                    End If
+
+                Loop Until getOutOfHere = True
+            End If
+
+            'calculation
+
+            If quitProgram = False Then
+                Console.Write(firstNumber & userInputOperation & secondNumber & "= ")
                 If userInputOperation = "+" Then
                     Console.WriteLine(firstNumber + secondNumber)
                 ElseIf userInputOperation = "-" Then
                     Console.WriteLine(firstNumber - secondNumber)
                 ElseIf userInputOperation = "*" Then
                     Console.WriteLine(firstNumber * secondNumber)
+                ElseIf userInputOperation = "/" Then
+                    Console.WriteLine(firstNumber / secondNumber)
                 Else
                     Console.WriteLine("Whoops. Something went wrong. Restart the program again...")
                 End If
-            End If
-
-            If quitProgram = False Then
-                Console.WriteLine()
-                Console.WriteLine(StrDup(80, "*"))
-                Console.WriteLine("NEW CALCULATION")
-                Console.WriteLine()
             End If
 
         Loop Until quitProgram = True
